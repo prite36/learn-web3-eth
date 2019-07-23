aa68c3e26e39a168aa4bea9837c217744c32a918
prite36 <spritelemon36@hotmail.com>
2019-07-23 16:39:30 +0700

    get balance

--- index.js
+++ index.js
@@ -3,27 +3,33 @@ import chalk from 'chalk'
 import Web3 from 'web3'
 import Koa from 'koa'
 import Router from 'koa-router'
+import cors from 'koa2-cors'
 const web3 = new Web3(new Web3.providers.WebsocketProvider(`wss://kovan.infura.io/ws/v3/${process.env.INFURA_SECRET}`));  // kovan
 const app = new Koa();
 const router = new Router();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 router.get('/', ctx => {
     ctx.body = 'nodejs'
 })
-router.get('/blockNumber',async ctx=> {
+router.get('/ethBlock',async ctx=> {
     try{
-        let data = {
+        ctx.body= {
             blockNumber: await web3.eth.getBlockNumber(),
             networkType: await web3.eth.net.getNetworkType()
         }
-        ctx.body= data
-    } catch (er){
-        console.log(er)
-    }
-    
+    } catch (err){
+        console.log(err)
+    }   
 })
-
+router.get('/balance/:myAddress',async ctx=> {
+    try{
+        let balance =  await web3.eth.getBalance(ctx.params.myAddress)
+        ctx.body = web3.utils.fromWei(balance, 'ether')
+    } catch (err){
+        console.log(err)
+    }   
+})
 router.get('/transactionsHistory/:myAddress', async ctx => {
     try {
         ctx.body = await getTransactionsByAccount(ctx.params.myAddress)
@@ -31,20 +37,30 @@ router.get('/transactionsHistory/:myAddress', async ctx => {
         console.log(er);       
     }
 })
-
+app.use(cors())
 app.use(router.routes())
 app.listen(port, () => {
     console.log(`listening on port ${port} ...`)
     console.log(chalk.yellow(`http://localhost:${port}`))
 })
-async function getTransactionsByAccount(myaccount) {
+
+const getTransactionsByAccount = async (myaccount) => {
     let endBlockNumber = await web3.eth.getBlockNumber()
     let startBlockNumber = endBlockNumber - 1000;
-    let result = await Promise.all(Array.from({length: endBlockNumber-(startBlockNumber-1)}, (_, k) => startBlockNumber + k).map(async value => {
-        var block =  await web3.eth.getBlock(value, true);
-        if (block != null && block.transactions != null) {
-            return block.transactions.filter( e => myaccount == "*" || myaccount == e.from || myaccount == e.to)
+    let result = await Promise.all(Array.from({length: endBlockNumber - (startBlockNumber - 1)}, (_, k) => startBlockNumber + k).map(async value => {
+        var { transactions, timestamp, difficulty } =  await web3.eth.getBlock(value, true);
+        if (transactions != null) {
+            return transactions
+            .filter( e => myaccount == "*" || myaccount == e.from || myaccount == e.to)
+            .map(element => {
+                return {
+                    ...element,
+                    gasPrice: web3.utils.fromWei(element.gasPrice, 'ether'),
+                    timestamp,
+                    difficulty}
+            })
         }
     }))
-    return result.filter(String)
-  }
+    // remove empty array and Flattening nested arrays
+    return result.filter(String).flat()
+}
